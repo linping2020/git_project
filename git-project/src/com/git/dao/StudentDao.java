bb504d79d2c8755d7cf676e49a5a782b16f45d67
linping2020 <3060717293>
2020-06-09 15:17:48 +0800

    添加工程

---
+++ git-project/src/com/git/dao/StudentDao.java
@@ -0,0 +1,18 @@
+package com.git.dao;
+
+public class StudentDao {
+	private int id;
+	private String name;
+	private String password;
+	
+	
+	
+	public StudentDao(int id, String name,String password) {
+		super();
+		this.id =id;
+		this.name=name;
+		this.password=password;
+		
+	}
+
+}
